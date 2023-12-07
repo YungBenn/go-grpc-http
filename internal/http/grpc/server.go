4cebbf9bad43bf7fd362abb320d99efb445c4cec
YungBenn <adisuryo22@gmail.com>
2023-12-07 16:53:04 +0700

    feat: add keepalive grpc server

--- internal/http/grpc/server.go
+++ internal/http/grpc/server.go
@@ -5,20 +5,35 @@ import (
 	"go-grpc-http/internal/postgresql"
 	"go-grpc-http/internal/service"
 	"net"
+	"time"
 
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/sirupsen/logrus"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/keepalive"
 	"google.golang.org/grpc/reflection"
 )
 
-func RunServer(log *logrus.Logger,db *pgxpool.Pool, address string) {
+var keep = keepalive.EnforcementPolicy{
+	MinTime:             5 * time.Second, 
+	PermitWithoutStream: true,            
+}
+
+var kasp = keepalive.ServerParameters{
+	MaxConnectionIdle:     15 * time.Second, 
+	MaxConnectionAge:      30 * time.Second, 
+	MaxConnectionAgeGrace: 5 * time.Second,  
+	Time:                  5 * time.Second,  
+	Timeout:               1 * time.Second,  
+}
+
+func RunServer(log *logrus.Logger, db *pgxpool.Pool, address string) {
 	listen, err := net.Listen("tcp", address)
 	if err != nil {
 		log.Errorf("failed to listen: %v", err)
 	}
 
-	server := grpc.NewServer()
+	server := grpc.NewServer(grpc.KeepaliveEnforcementPolicy(keep), grpc.KeepaliveParams(kasp))
 	repo := postgresql.NewRepository(db)
 	srv := service.NewCarServiceServer(log, repo)
 	pb.RegisterCarServiceServer(server, srv)
